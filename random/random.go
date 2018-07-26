ecd3877071314c4eeb79e65c045d0a7c4f4c37af
Weiqiang Su <weiqiang@dreamxu.com>
2018-07-26 14:50:03 +0800

    init random package

---
+++ random/random.go
@@ -0,0 +1,44 @@
+package random
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	LettersAndNumbersBytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	LettersBytes           = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	NumbersBytes           = "0123456789"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func StringWithBaseBytes(length int, baseBytes string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = baseBytes[rand.Intn(len(baseBytes))]
+	}
+	return string(b)
+}
+
+func String(length int) string {
+	return StringWithBaseBytes(length, LettersAndNumbersBytes)
+}
+
+func Letters(length int) string {
+	return StringWithBaseBytes(length, LettersBytes)
+}
+
+func Numbers(length int) string {
+	return StringWithBaseBytes(length, NumbersBytes)
+}
+
+func EmailWithLengthAndDomainName(length int, domainName string) string {
+	return String(length) + "@" + domainName
+}
+
+func Email() string {
+	return EmailWithLengthAndDomainName(20, "example.com")
+}
